b80dc9f7e9bb03fbbbda13911003f4acdc0fcfb4
marsmen <marsmen@naver.com>
2016-09-27 04:22:54 +0900

    재작업 및 배경 샘플 적용

--- 06-Build3/GirlAndGhost/Assets/TextureChangeTest.cs
+++ 06-Build3/GirlAndGhost/Assets/TextureChangeTest.cs
@@ -16,38 +16,38 @@ public class TextureChangeTest : MonoBehaviour {
 	void Start () {
         
         SkeletonRenderer skeletonRenderer = GetComponent<SkeletonRenderer>();
-
         Attachment prevAttachment = skeletonRenderer.skeleton.GetAttachment(slot, "eye/type_00/eye_c");
-        
         Attachment attachment = skeletonRenderer.skeleton.Data.AddUnitySprite(slot, sprite, skin);
-
         skeletonRenderer.skeleton.SetAttachment(slot, sprite.name);
-       
         
-       // SkeletonAnimation skeletonAnimation = GetComponent<SkeletonAnimation>();
-        //skeletonAnimation.skeleton.SetAttachment("eye/type_00/eye_c", "eye/type_01/eye_c");
-        //skeletonAnimation.skeleton.SetAttachment("eye/type_00/eye_c_b", "eye/type_01/eye_c_b");
-        
+        /*
+        SkeletonAnimation skeletonAnimation = GetComponent<SkeletonAnimation>();
+        skeletonAnimation.skeleton.SetAttachment("eye/type_00/eye_c", "eye/type_01/eye_c");
+        skeletonAnimation.skeleton.SetAttachment("eye/type_00/eye_c_b", "eye/type_01/eye_c_b");
+        */
         print("skin");
         //skeletonAnimation.skeleton.SetAttachment("Base/Head/Back_Hair", "hair/type_02/Back_Hair");
         //skeletonAnimation.skeleton.SetAttachment("Base/Head/Front_Hair", "hair/type_02/Front_Hair");
         
-        /*
         var type = AttachmentType.region;
 
         if (type == AttachmentType.region) {
             //RegionAttachment prevregionAttachment = (RegionAttachment)prevAttachment;
             RegionAttachment regionAttachment = (RegionAttachment)attachment;
-           // regionAttachment.x = prevregionAttachment.x;
-           // regionAttachment.y = prevregionAttachment.y;
+            //regionAttachment.x = prevregionAttachment.x;
+            //regionAttachment.y = prevregionAttachment.y;
+            regionAttachment.x = 0.25f;
+            regionAttachment.y = 0.06f;
             //regionAttachment.scaleX = prevregionAttachment.scaleX;
-           // regionAttachment.scaleY = prevregionAttachment.scaleY;
+            //regionAttachment.scaleY = prevregionAttachment.scaleY;
+            regionAttachment.scaleX = 0.75f;
+            regionAttachment.scaleY = 0.7f;
             regionAttachment.rotation = 270;
             //regionAttachment.width = prevregionAttachment.width;
             //regionAttachment.height = prevregionAttachment.height;
             regionAttachment.UpdateOffset();
         }
-         */
+        
 	}
 	
 }
